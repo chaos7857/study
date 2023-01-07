542d2f72b21f00e3c80c80092ac904b22a20bfe0
叨叨 <121782246+VargantChaos@users.noreply.github.com>
2023-01-07 21:34:46 +0800

    Add files via upload

---
+++ 条件.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int age = 10;
+    if (age>18)
+    {
+        printf("hello ");
+        /* code */
+    }
+    else
+        printf("bye bye ");
+    
+    printf("world\n");
+
+
+    return 0;
+}
