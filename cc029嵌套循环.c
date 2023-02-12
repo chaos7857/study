5851b347cf7404868470811a7a04dcc19af53816
叨叨 <121782246+VargantChaos@users.noreply.github.com>
2023-02-12 14:59:42 +0800

    Add files via upload

---
+++ cc029嵌套循环.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            printf("i = %d, j = %d\n", i, j);
+        }
+        
+    }
+    
+    return 0;
+}
