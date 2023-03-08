e03526e799658994cc21d38f5a4ba44e054f7101
叨叨 <121782246+VargantChaos@users.noreply.github.com>
2023-03-08 23:01:38 +0800

    Add files via upload

---
+++ standard.py
@@ -0,0 +1,13 @@
+import requests
+
+url_st = "http://dxx.ahyouth.org.cn/api/peopleRankStage?table_name=reason_stage245&level1=%E7%9B%B4%E5%B1%9E%E9%AB%98%E6%A0%A1&level2=%E5%90%88%E8%82%A5%E5%B7%A5%E4%B8%9A%E5%A4%A7%E5%AD%A6&level3=%E7%AE%A1%E7%90%86%E5%AD%A6%E9%99%A2%E5%9B%A2%E5%A7%94&level4=%E4%BF%A1%E6%81%AF%E7%AE%A1%E7%90%86%E4%B8%8E%E4%BF%A1%E6%81%AF%E7%B3%BB%E7%BB%9F%E4%B8%93%E4%B8%9A2022%E7%BA%A71%E7%8F%AD"
+
+response_st = requests.get(url_st).json()
+list_st = []
+d = 0
+for name in response_st['list']['list']:
+    # print(arr['username'])
+    list_st.append(name['username'])
+    d += 1
+print(list_st)
+print(len(list_st))
