8e27abd8535d8c6f6996c9c2fbb6c901a95ea544
Cc <173192386@qq.com>
2024-02-28 19:51:25 +0800

    autowork

--- d2matplotlib/rw_visual.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-from random_walk import RandomWalk
-rw = RandomWalk(600)
-rw.walk()
-fig, ax = plt.subplots()
-ax.scatter(rw.x,rw.y, c=rw.y, cmap=plt.cm.Greens, edgecolors='none')
-ax.set_title("randomWalk")
-plt.show()
